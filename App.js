8d515186d498b26bff39131c52c4402c4d973436
orhan <67384523+reguest@users.noreply.github.com>
2022-06-14 16:05:04 +0300

    14.06.2022 veri çekme

--- App.js
+++ App.js
@@ -7,28 +7,81 @@
  */
 
 import React from 'react';
-import {View,Text,StatusBar,TouchableOpacity} from 'react-native';
+import { View, Text, StatusBar, TouchableOpacity, ActivityIndicator, StyleSheet } from 'react-native';
 
 
 
 export default class App extends React.Component {
 
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      dataSource: null,
+    }
+  }
 
-render(){
-  return(
-    <View>
-      <Text>orhan</Text>
-      {/* https://reactnative.dev/movies.json */}
-    </View>
-  )
+  componentDidMount() {
+
+
+    return fetch('https://reactnative.dev/movies.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson.movies,
+        })
+
+      })
+
+      .catch((error) => {
+        console.log(error)
+      });
+  }
+
+  render() {
+
+    if (this.state.isLoading) {
+      return (
+        <View >
+          <ActivityIndicator />
+        </View>
+      )
+    } else {
+      let movies = this.state.dataSource.map((val, key) => {
+        return <View key={key} style={StyleSheet.item}>
+          <Text>{val.title}</Text>
+        </View>
+      });
+      return (
+        <View >
+          {movies}
+        </View>
+      );
+    }
+
+
+
+  }
+
+
 }
- 
-
-}
 
 
+const styles = StyleSheet.create({
 
+
+  item: {
+    flex: 1,
+    alignItems: 'stretch',
+    margin: 10,
+    justifyContent: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#eee',
+  }
+});
 
 
 
